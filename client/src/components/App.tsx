4abbe075e7a31d380d9eb1d63faac0decdc8fda7
Sxcha <sservant@z2r4p5.42lyon.fr>
2025-12-05 01:06:07 +0100

    init/relword

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -7,7 +7,7 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
-import Test from "../pages/test";
+import Relword from "../pages/Relword";
 
 export default function App() {
   return (
@@ -25,6 +25,7 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/rel" element={<Relword />} />
           </Routes>
         </BrowserRouter>
     </AuthProvider>
