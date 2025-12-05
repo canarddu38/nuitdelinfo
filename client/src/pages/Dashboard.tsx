9a0399792b74cba36a9eb708264aceff72bbdbc7
Mathis Bichet <mbichet@z2r4p3.42lyon.fr>
2025-12-05 06:22:02 +0100

    fix

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -31,7 +31,7 @@ export default function Dashboad() {
     navigate("../reco");
   }
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
+    <div style={{ position: 'relative', width: '100%', height: '100vh'}}>
       
       {/* Le composant d'arrière-plan */}
       <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
